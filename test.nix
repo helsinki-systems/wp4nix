2a3d82a816f548e8d75cdc6e0196d5391222d6c3
ajs124 <git@ajs124.de>
2020-03-24 01:51:19 +0100

    enable theme tests

--- test.nix
+++ test.nix
@@ -15,16 +15,16 @@ in
     wp-piwik
     wp-smtp
   ])
-  #++ (with wp4nix.themes; [
-  #  blogmagazine
-  #  hestia
-  #  kawi
-  #  newsmag
-  #  rocked
-  #  twentyfifteen
-  #  twentytwenty
-  #  vmagazine-lite
-  #])
+  ++ (with wp4nix.themes; [
+    blogmagazine
+    hestia
+    kawi
+    newsmag
+    rocked
+    twentyfifteen
+    twentytwenty
+    vmagazine-lite
+  ])
   ++ (with wp4nix.languages; [
     de_DE
     en_GB
