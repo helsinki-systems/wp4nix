531714acf0b85c2732828080a74cbe1290601074
Janne Heß <janne@hess.ooo>
2021-09-20 20:04:14 +0200

    Add scopes and more `extends`
    This essentially rewrites default.nix.
    First off, we start using the magic of scopes and create a scope for the
    wordpress packages. This scope is still extensible but also contains
    utilitiy functions now, namely `mkWordpressDerivation`,
    `mkOfficialWordpressDerivation`, `filterWPString`, and `fetchWordpress`.
    
    This way, we are exposing almost all functionality to third-party
    software which can extend the package set and use the existing
    functionality.
    
    `mkWordpressDerivation` now properly calls the nixpkgs hooks and adds
    the package type into the `pname`.
    
    We also add `extend` functions to all package groups (meaning you can
    now do `plugins.extend` instead of `//`ing it and add some overrides
    from helsinki here.

--- default.nix
+++ default.nix
@@ -1,45 +1,117 @@
-{ fetchsvn, unzip, lib, stdenvNoCC, wp-cli, gettext }: with lib;
-let packages = (self:
+{ lib, newScope }:
+
+let packages = self:
   let
-    json = {
-      plugins = builtins.fromJSON (readFile ./plugins.json);
-      themes = builtins.fromJSON (readFile ./themes.json);
-      languages = builtins.fromJSON (readFile ./languages.json);
-      pluginLanguages = builtins.fromJSON (readFile ./pluginLanguages.json);
-      themeLanguages = builtins.fromJSON (readFile ./themeLanguages.json);
+    generatedJson = {
+      plugins = builtins.fromJSON (builtins.readFile ./plugins.json);
+      themes = builtins.fromJSON (builtins.readFile ./themes.json);
+      languages = builtins.fromJSON (builtins.readFile ./languages.json);
+      pluginLanguages = builtins.fromJSON (builtins.readFile ./pluginLanguages.json);
+      themeLanguages = builtins.fromJSON (builtins.readFile ./themeLanguages.json);
     };
-    filterFileName = f: builtins.replaceStrings [ " " "," "/" "&" ";" ''"'' "'" "$" ":" "(" ")" "[" "]" "{" "}" "|" "*" "\t" ] [ "_" "." "." "" "" "" "" "" "" "" "" "" "" "" "" "-" "" "" ] f;
-    fetch = t: v: fetchsvn {
-      inherit (v) rev sha256;
-      url = if t == "plugins" || t == "themes" then
-        "https://" + t + ".svn.wordpress.org/" + v.path
-      else if t == "languages" then
-        "https://i18n.svn.wordpress.org/core/" + v.version + "/" + v.path
-      else if t == "pluginLanguages" then
-        "https://i18n.svn.wordpress.org/plugins/" + v.path
-      else if t == "themeLanguages" then
-        "https://i18n.svn.wordpress.org/themes/" + v.path
+
+  in {
+    # Create a generic WordPress package. Most arguments are just passed
+    # to `mkDerivation`. The version is automatically filtered for weird characters.
+    mkWordpressDerivation = self.callPackage ({ stdenvNoCC, lib, filterWPString, gettext, wp-cli }:
+      { type, pname, version, ... }@args:
+        assert lib.any (x: x == type) [ "plugin" "theme" "language" "pluginLanguage" "themeLanguage" ];
+        stdenvNoCC.mkDerivation ({
+          pname = "wordpress-${type}-${pname}";
+          version = filterWPString version;
+
+          dontConfigure = true;
+          dontBuild = true;
+
+          installPhase = ''
+            runHook preInstall
+            cp -R ./. $out
+            runHook postInstall
+          '';
+        } // lib.optionalAttrs (type == "language" || type == "pluginLanguage" || type == "themeLanguage") {
+          nativeBuildInputs = [ gettext wp-cli ];
+          dontBuild = false;
+          buildPhase = ''
+            runHook preBuild
+
+            find -name '*.po' -print0 | while IFS= read -d "" -r po; do
+              msgfmt -o $(basename "$po" .po).mo "$po"
+            done
+            wp i18n make-json .
+            rm *.po
+
+            runHook postBuild
+          '';
+        } // removeAttrs args [ "type" "pname" "version" ])) {};
+
+    # Create a derivation from the official wordpress.org packages.
+    # This takes the type, the pname and the data generated from the go tool.
+    mkOfficialWordpressDerivation = self.callPackage ({ mkWordpressDerivation, fetchWordpress }:
+      { type, pname, data }:
+      mkWordpressDerivation {
+        inherit type pname;
+        version = data.version;
+
+        src = fetchWordpress type data;
+      }) {};
+
+    # Filter out all characters that might occur in a version string but that that are not allowed
+    # in store paths.
+    filterWPString = builtins.replaceStrings [ " " "," "/" "&" ";" ''"'' "'" "$" ":" "(" ")" "[" "]" "{" "}" "|" "*" "\t" ] [ "_" "." "." "" "" "" "" "" "" "" "" "" "" "" "" "-" "" "" ];
+
+    # Fetch a package from the official wordpress.org SVN.
+    # The data supplied is the data straight from the go tool.
+    fetchWordpress = self.callPackage ({ fetchsvn }: type: data: fetchsvn {
+      inherit (data) rev sha256;
+      url = if type == "plugin" || type == "theme" then
+        "https://" + type + "s.svn.wordpress.org/" + data.path
+      else if type == "language" then
+        "https://i18n.svn.wordpress.org/core/" + data.version + "/" + data.path
+      else if type == "pluginLanguage" then
+        "https://i18n.svn.wordpress.org/plugins/" + data.path
+      else if type == "themeLanguage" then
+        "https://i18n.svn.wordpress.org/themes/" + data.path
       else
-        throw "invalid fetch type";
-    };
-    mkPkg = type: pname: value: stdenvNoCC.mkDerivation ({
-      inherit pname;
-      version = filterFileName value.version;
-      src = fetch type value;
-      installPhase = ''
-        cp -R ./. $out
-      '';
-    } // optionalAttrs (type == "languages" || type == "pluginLanguages" || type == "themeLanguages") {
-      nativeBuildInputs = [ gettext wp-cli ];
-      buildPhase = ''
-        find -name '*.po' -print0 | while IFS= read -d "" -r po; do
-          msgfmt -o $(basename "$po" .po).mo "$po"
-        done
-        wp i18n make-json .
-        rm *.po
+        throw "fetchWordpress: invalid package type ${type}";
+    }) {};
+
+  } // lib.mapAttrs (type: pkgs: lib.makeExtensible (_: lib.mapAttrs (pname: data: self.mkOfficialWordpressDerivation { type = lib.removeSuffix "s" type; inherit pname data; }) pkgs)) generatedJson;
+
+# This creates an extensible scope and immediately extends it with our custom
+# package overrides.
+in (lib.makeExtensible (_: (lib.makeScope newScope packages))).extend (selfWP: superWP: {
+
+  plugins = superWP.plugins.extend (selfPlugins: superPlugins: {
+    # Fix the plugin caching store paths
+    visualcomposer = superPlugins.visualcomposer.overrideAttrs (oA: {
+      postInstall = ''
+        ${oA.postInstall or ""}
+        rm -r $out/cache
       '';
     });
-  in
-    genAttrs [ "plugins" "themes" "languages" "pluginLanguages" "themeLanguages" ] (t: mapAttrs (mkPkg t) json."${t}")
-);
-in makeExtensible packages
+
+    # Add nix syntax support
+    prismatic = superWP.callPackage ({ fetchurl }: superPlugins.prismatic.overrideAttrs (oA: {
+      postInstall = ''
+        ${oA.postInstall or ""}
+
+        cp ${fetchurl {
+          url = "https://raw.githubusercontent.com/PrismJS/prism/v1.25.0/components/prism-nix.js";
+          sha256 = "1ajw7pdnppgyar3v42nraxm0f4rzyqb5fw75ych8q3x94v4klgc7";
+        }} $out/lib/prism/js/lang-nix.js
+
+        # Allow loading the js
+        substituteInPlace $out/inc/resources-enqueue.php \
+          --replace "'lang-none'," "'lang-none', 'lang-nix'," \
+          --replace "'language-none'," "'language-none', 'language-nix',"
+
+        # Add the language into the editor
+        sed -i "/Language\.\./a { label : 'Nix', value : 'nix' }," $out/js/blocks-prism.js
+        sed -i "/Language\.\./a { label : 'Nix', value : 'nix' }," $out/js/buttons-prism.js
+      '';
+    })) {};
+  });
+
+  #themes = superWP.plugins.extend (selfThemes: superThemes: {
+  #});
+})
